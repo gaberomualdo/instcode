02a9cb59ece2a97049d286fffc6031d64d91a1bc
xtrp <36894883+xtrp@users.noreply.github.com>
2018-08-12 13:21:08 -0400

    added challenges list page

---
+++ assets/js/challengeslist.js
@@ -0,0 +1,8 @@
+var difficulties = ["easy","medium","hard"];
+var difficulty = _getQueryVariable("d");
+var all_challenges_of_difficulty = _challenges_list[difficulties.indexOf(difficulty)];
+
+all_challenges_of_difficulty.forEach(function(item,index){
+  $("div.challenges_list").addClass(difficulty);
+  $("div.challenges_list").append("<a href='challenge.html?d=" + difficulty + "&q=" + index + "'>" + item.name + "</a>")
+});
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,3 @@
 $(".button_container button").on("click",function(){
-  window.location.href = "challenge.html?d=" + $(this).attr("class") + "&q=" + Math.floor(Math.random() * 5);
+  window.location.href = "challengeslist.html?d=" + $(this).attr("class");
 });
